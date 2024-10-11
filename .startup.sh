2527956fded26998c77e01284de5506ed5689afe
Nitin Garg <gargnitin@google.com>
2024-10-11 06:15:41 +0000

    untested uninteresting change

--- .startup.sh
+++ .startup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+set -x
+set -e
+
 #install/enable google-drive
-sudo glinux-add-repo drive-fs-all stable && sudo apt update && sudo apt install -y drive-fs && /opt/google/drive-file-stream/drive_fs 2>/dev/null && systemctl --user enable --now google-drive-fs && ls ~/DriveFileStream/
+sudo glinux-add-repo drive-fs-all stable && sudo apt update -y && sudo apt install -y drive-fs && /opt/google/drive-file-stream/drive_fs 2>/dev/null && systemctl --user enable --now google-drive-fs && ls ~/DriveFileStream/
 
